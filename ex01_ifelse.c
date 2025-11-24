0de0f9aad4c347fab12c5c6d0de0167773d7c323
luizhenriquesouza13-pixel <luiz.henrique.souza13@escola.pr.gov.br>
2025-11-24 08:21:09 -0300

    Create ex01_ifelse.c

---
+++ ex01_ifelse.c
@@ -0,0 +1,15 @@
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se é positivo, negativo ou zero
+if numero > 0:
+    print("O número é positivo")
+elif numero < 0:
+    print("O número é negativo")
+else:
+    print("O número é zero")
+
+# Verifica se é par ou ímpar
+if numero % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
