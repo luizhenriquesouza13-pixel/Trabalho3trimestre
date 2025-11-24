7e46773fd7c8e50c5b16231515c1edce90aedcdb
luizhenriquesouza13-pixel <luiz.henrique.souza13@escola.pr.gov.br>
2025-11-24 08:22:10 -0300

    Update ex01_ifelse.c

--- ex01_ifelse.c
+++ ex01_ifelse.c
@@ -1,15 +1,26 @@
-numero = int(input("Digite um número inteiro: "))
+#include <stdio.h>
 
-# Verifica se é positivo, negativo ou zero
-if numero > 0:
-    print("O número é positivo")
-elif numero < 0:
-    print("O número é negativo")
-else:
-    print("O número é zero")
+int main() {
+    int numero;
 
-# Verifica se é par ou ímpar
-if numero % 2 == 0:
-    print("O número é par")
-else:
-    print("O número é ímpar")
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    // Verifica se é positivo, negativo ou zero
+    if (numero > 0) {
+        printf("O numero eh positivo.\n");
+    } else if (numero < 0) {
+        printf("O numero eh negativo.\n");
+    } else {
+        printf("O numero eh zero.\n");
+    }
+
+    // Verifica se é par ou ímpar
+    if (numero % 2 == 0) {
+        printf("O numero eh par.\n");
+    } else {
+        printf("O numero eh impar.\n");
+    }
+
+    return 0;
+}
